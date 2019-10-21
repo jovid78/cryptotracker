84425dc8e64142e34c0f1c7ec0af566e56267d73
Jose Vidal <josemvidal78@gmail.com>
2019-10-20 20:01:57 -0400

    Refine CMC request params Add Mock data folder to gitignore

--- src/services/CoinService.js
+++ src/services/CoinService.js
@@ -2,17 +2,12 @@ import axios from 'axios';
 import * as config from '../config/servicesConfig';
 
 const COIN_ENDPOINT = '/v1/cryptocurrency/listings/latest';
-const PARAMS = `?start=1&limit=1000&convert=USD&CMC_PRO_API_KEY=${config.CMC_API_KEY}`;
+const PARAMS = `?start=1&limit=200&convert=USD&sort=market_cap&aux=cmc_rank&CMC_PRO_API_KEY=${config.CMC_API_KEY}`;
 
 export class CoinService {
 
   async fetchCoinData() {
     const url = `${config.CMC_DOMAIN}${COIN_ENDPOINT}${PARAMS}`;
-    // const headers = {
-    //   [config.CMC_AUTH_HEADER]: config.CMC_API_KEY,
-    //   'Content-Type': 'application/json'
-    // };
-
     const response = await axios.get(url)
 
     console.log(response)
